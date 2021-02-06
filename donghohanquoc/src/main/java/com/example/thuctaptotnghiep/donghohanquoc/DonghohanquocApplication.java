c7a9ba6068378efceda8a63f16df62cf026f14ca
= <=>
2021-02-07 05:52:55 +0700

    first commit

---
+++ donghohanquoc/src/main/java/com/example/thuctaptotnghiep/donghohanquoc/DonghohanquocApplication.java
@@ -0,0 +1,13 @@
+package com.example.thuctaptotnghiep.donghohanquoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonghohanquocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonghohanquocApplication.class, args);
+    }
+
+}
---
+++ donghohanquoc/src/test/java/com/example/thuctaptotnghiep/donghohanquoc/DonghohanquocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thuctaptotnghiep.donghohanquoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonghohanquocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
