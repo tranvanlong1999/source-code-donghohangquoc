0e58e6a92955a26db39e82140a54c4d48c7af40c
= <=>
2021-02-22 21:38:42 +0700

    update moi nhat111

--- donghohanquoc/src/main/java/com/example/thuctaptotnghiep/donghohanquoc/Repository/BrandRepository.java
+++ donghohanquoc/src/main/java/com/example/thuctaptotnghiep/donghohanquoc/Repository/BrandRepository.java
@@ -4,4 +4,5 @@ import com.example.thuctaptotnghiep.donghohanquoc.Model.Entity.BrandEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BrandRepository extends JpaRepository<BrandEntity,Integer> {
+
 }
