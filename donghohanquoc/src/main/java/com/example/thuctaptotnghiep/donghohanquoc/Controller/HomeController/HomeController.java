11061871e231748298cebad640d572c6bd72065d
VTI\long.tranvan <long.tranvan>
2021-03-18 17:45:57 +0700

    update login

--- donghohanquoc/src/main/java/com/example/thuctaptotnghiep/donghohanquoc/Controller/HomeController/HomeController.java
+++ donghohanquoc/src/main/java/com/example/thuctaptotnghiep/donghohanquoc/Controller/HomeController/HomeController.java
@@ -8,6 +8,7 @@ import com.example.thuctaptotnghiep.donghohanquoc.Model.Output.UserOutput;
 import com.example.thuctaptotnghiep.donghohanquoc.Service.ProductService;
 import com.example.thuctaptotnghiep.donghohanquoc.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -20,47 +21,48 @@ import java.util.List;
 
 @Controller
 public class HomeController {
+    /*@Value("${error.message}")
+    private String errorMessage;*/
+
+
+    private  String errorMessage="Email không tồn tại";
     @Autowired
     private UserService userService;
     @Autowired
     private ProductService productService;
+
     @GetMapping("/home")
     public String home(Model model) {
-        List<ProductOutput> productOutputList= productService.getListProduct();
-        model.addAttribute("productInfo",productOutputList);
+        List<ProductOutput> productOutputList = productService.getListProduct();
+        model.addAttribute("productInfo", productOutputList);
         return "index-3";
     }
 
     @GetMapping("/login-register")
-    public String login(@RequestParam("message") String message, Model model) {
-        LoginInput loginInput= new LoginInput();
-        UserInput userInput= new UserInput();
-        model.addAttribute("message",message);
-        model.addAttribute("userInput",userInput );
-        model.addAttribute("loginInput",loginInput);
+    public String login(Model model){
+        LoginInput loginInput = new LoginInput();
+        UserInput userInput = new UserInput();
+        model.addAttribute("errorMessage","");
+        model.addAttribute("userInput", userInput);
+        model.addAttribute("loginInput", loginInput);
         return "login-register";
     }
+
     @PostMapping("/login/user")
-    public RedirectView showhome(@ModelAttribute("loginInput") LoginInput loginInput,RedirectAttributes redirectAttributes)
-    {
-        UserOutput userOutput =userService.checkLogin(loginInput);
-
-        if(userOutput.getEmail()==null)
-        {
-            redirectAttributes.addAttribute("message","email k ton tai");
-            return new RedirectView("login-register");
+    public String showhome(@ModelAttribute("loginInput") LoginInput loginInput, Model model) {
+        UserOutput userOutput = userService.checkLogin(loginInput);
+        System.out.println(loginInput);
+        if (userOutput.getEmail() == null) {
+            model.addAttribute("errorMessage",errorMessage);
+            return "redirect:/login-register";
+        } else {
+            return "redirect:/home";
         }
-        else{
-           // return "redirect:/home";
-            return null;
-
-        }
-
 
     }
+
     @PostMapping("/register/user/bycustomer")
-    public String registeruser(@ModelAttribute("userInput") UserInput userInput)
-    {
+    public String registeruser(@ModelAttribute("userInput") UserInput userInput) {
         userService.createUserByAdmin(userInput);
         return "redirect:/login-register";
     }
